acfb36f58b91ca98bed8cf59a010782d4e75bb1a
JD <jdmejia712@gmail.com>
2021-11-10 13:05:13 -0500

    commit tree 28d219e74e77b94c434a670a2c6039114867826e

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
+  const base_URL = 'http://localhost:3000/movies';
+  const inputForm = document.querySelector('form');
   
+  
+  inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const textbox = document.querySelector('#searchByID');
+    console.log(textbox.value);
+
+   fetch(base_URL+ `/${textbox.value}`)
+    .then(result=> result.json())
+    .then(data=>{
+        const title = document.querySelector('#movieDetails h4');
+        const summary = document.querySelector('#movieDetails p');
+  
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+        textbox.value = '';
+            })
+        });
 }
 
 document.addEventListener('DOMContentLoaded', init);
